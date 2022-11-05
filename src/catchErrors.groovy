36ebb656250ce6a0e09e95b7b78a6b149518d151
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-05 21:40:27 +0300

    Delete catchErrors.groovy

--- src/catchErrors.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-    try { if (MODULES == '') {
-            println('Please select one Utility checkbox')
-            error('autoStop') } }
-        catch(e) { if (e.message == 'autoStop') {
-            currentBuild.result = 'ABORTED' }
-        throw e }
-}
