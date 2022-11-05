f6a2f7ad63d9bb737d6d4a82ff9157db76c070d4
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-06 00:02:27 +0300

    Add files via upload

--- vars/libpatch.groovy
+++ vars/libpatch.groovy
@@ -5,13 +5,6 @@ def set_env() {
     servers = yaml_cfg.server
 }
 def prepare() {
-    node ( "OW" ) {
-        // checkout our git
-        checkout changelog: false, poll: false, scm: [
-            $class: 'GitSCM', 
-            branches: [[name: '*/main']], 
-            extensions: [], 
-            userRemoteConfigs: [[credentialsId: 'jenkins_gitlab_token', url: 'http://10.255.252.160/prime/js.git']]]
         def vm_creds = [
             [path: 'secrets/creds/vsphere', secretValues: [
                 [envVar: 'j_pwd', vaultKey: 'password']]]
@@ -20,10 +13,8 @@ def prepare() {
                 SESSION_ID_TMP = sh (script: """curl -s -k -X POST https://vcenter.internal/rest/com/vmware/cis/session -u  'jenkucs_sa':'${j_pwd}'""", returnStdout: true)
             }
             SESSION_ID = SESSION_ID_TMP.drop(10).take(32)
-        }
 }
 def power(String action, String reg) {
-    node ( "OW" ) {
         switch (action) {
             case ('shutdown'):
                 for (server in tools.getVmForRegion(region: "${reg}").split(' ')) {
@@ -44,22 +35,18 @@ def power(String action, String reg) {
                 """ }
             break
         }
-    }
 }
 
 def gates(String action, String reg) {
     for (server in tools.getVmForRegion(region: "${reg}").split(' ')) {
-        node ( "OW" ) {
             sh """
             #curl -s -k -X POST -H "vmware-api-session-id: ${SESSION_ID}" "https://vcenter.internal/rest/vcenter/vm/{${server}}/hardware/ethernet/{4000}/${action}"
             curl -s -k -X GET -H "vmware-api-session-id: ${SESSION_ID}" "https://vcenter.internal/rest/vcenter/vm/{${server}}/hardware/ethernet"
             """
-        }
     }
 }
 def rdp(String reg) {
     for (server in tools.getNameForRegion(region: "${reg}").split(' ')) {
-        node ( "OW" ) {
             server_user='jenkucs_sa'
             sh """
             #scp ./win/rdpsessions.ps1 ${server_user}@${server}:/home/${server_user}/rdpsessions.ps1
@@ -67,7 +54,6 @@ def rdp(String reg) {
             ssh ${server_user}@${server} "powershell "./\\\\rdpsessions.ps1 ${server}""
             """
         }
-    }
 }
 
 
