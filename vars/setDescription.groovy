eadb13befc931f3b1ed9d168766091647300b6ec
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-07 00:51:01 +0300

    Add files via upload

--- vars/setDescription.groovy
+++ vars/setDescription.groovy
@@ -1,14 +1,11 @@
 def call() {
     //build description inside web UI
     switch (JOB_BASE_NAME) {
-            case ["fis", "utility_fis", "tid_man", "mms_eod", "palmera"]:
+            case ["fis", "fis_util", "tid_man", "mms_eod", "palmera"]:
                 currentBuild.description = "node: ${NODE_NAME}\nsvn: ${SVN}; version: ${VERSION}\nbuild_number: ${BUILD_NUMBER}"
                 break
-            case ["gemalto", "eracom", "svn_nexus"]:
-                currentBuild.description = "node: ${NODE_NAME}\nbuild_number: ${BUILD_NUMBER}"
-                break
             default:
-                println ("TBD")
+                currentBuild.description = "node: ${NODE_NAME}\nbuild_number: ${BUILD_NUMBER}"
         }
     
 }
