4b2503daaded204a7ccc037e65327a13f8fd605f
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-14 00:10:11 +0300

    Add files via upload

--- vars/pth.sh
+++ vars/pth.sh
@@ -4,10 +4,11 @@ set +e
 pushd $1; java -jar BIN/xsltc.jar -i "$2/$3/$4.$5" -o "$2/$3/$4.xml" -l stdout.log -x BIN/pth2lst.xslt
 cat stdout.log >> ktr_xml.log
 pushd BIN; echo "---------- $4.xml ----------" >> CheckSql.log;
-java -jar checkersql.jar "../$2/$3/$4.xml"
-if [ -z "$3" ]; then 
-  popd +0; zip -q -u $2.zip ./$2/*.xml
+if [ -z "$3" ]; then
+  java -jar checkersql.jar "../$2/$4.xml"
+  popd +0; zip -q -u $2.zip ./$2/"$4".xml
 else
-  popd +0; zip -q -u $2.zip ./$2/$3/*.xml
+  java -jar checkersql.jar "../$2/$3/$4.xml"
+  popd +0; zip -q -u $2.zip ./$2/$3/"$4".xml
 fi
 # true - for testing only. In prod - comment it
