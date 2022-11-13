dadee26cf66d0a97bfff54dfae08877ebf471e19
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-14 01:35:28 +0300

    Add files via upload

--- vars/uploadNexus.groovy
+++ vars/uploadNexus.groovy
@@ -1,3 +1,4 @@
+import org.apache.commons.io.FilenameUtils
 def call(String path) {
     dir (path) {
         def nexus_creds = [
@@ -5,30 +6,20 @@ def call(String path) {
         [envVar: 'nexus_pwd', vaultKey: 'password']]]]
         //level 1 - group folder
         lvl1 = listDir(path)
-        //println lvl1
         lvl2=[]
-        lvl1_lvl2=[]
-        all=[]
-        //to delete ./svn folder
-        sh "rm -rf ./.svn"
+        //to delete ./svn folder recursively
+        sh "find . -type d -name .svn -exec rm -rf {} +"
         //level 2 - project folder
         for (int i = 0; i < lvl1.size(); i++) {
         lvl2[i] = listDir("${path}/${lvl1[i]}/")
-        //println lvl2[i]
             for (int j=0; j < lvl2[i].size(); j++) {
-            lvl1_lvl2[j] = "${WORKSPACE}/${path}/${lvl1[i]}/${lvl2[i][j]}"
-            //println lvl1_lvl2[j]
                 //level 3 - target folder: workspace/svn_path/lvl1/lvl2/LVL3
-                proc1 = sh (returnStdout: true, script: "rm -rf ./.svn ; ls ${lvl1_lvl2[j]}")
-                lvl3 = proc1.split().toList()
-                //println lvl3 
+                lvl3=listDir("${path}/${lvl1[i]}/${lvl2[i][j]}")
                 // all = workspace/svn_path/lvl1/lvl2/lvl3
                 for (int k=0; k < lvl3.size(); k++) {
-                all ="${lvl1_lvl2[j]}/${lvl3[k]}"
-                //println all
                     wrap([$class: 'VaultBuildWrapper', vaultSecrets: nexus_creds]) {
                     sh """
-                    pushd ${all}
+                    pushd ${lvl1[i]}/${lvl2[i][j]}/${lvl3[k]}
                     if [ -z "\$(ls -A )" ]; then echo "Empty"
                     else
                     zip -r -q ${lvl3[k]}.zip *
