8af6e1ada6652356049426a1e46a58fa87dfbefa
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-16 23:21:20 +0300

    Add files via upload

--- vars/mmMake.groovy
+++ vars/mmMake.groovy
@@ -4,25 +4,12 @@ def call(String path, String arch) {
         case ('units') :
             sample=mm.split(', ') //'myizip_z', 'microx_t', 'cyassl-3.2.0', 'axcoder'
             for (int i=0; i < 2; i++) { //myizip_z', 'microx_t
-                switch (arch) {
-                    case ('x64') :
-                    mmX64(sample[i])
-                    break
-                    default : //'atol' currently
-                    mmX86(sample[i])
-                }//switch (arch) finished
-            }//loop for sample[i] finished
+                arch=='x64' ? mmX64(sample[i]) : mmX86(sample[i]) }
         break
         case ('units/microx_t/samples') :
             sample1=mmm.split(', ') //'microp', 'ucs_mm', 'ucs_ms', 'ucs_dt'
-            for (int i=0; i < sample1.size(); i++) {
-                switch (arch) {
-                    case ('x64') :
-                    mmX64(sample1[i])
-                    default : // 'atol' currently
-                    mmX86(sample1[i])
-                } //switch (arch) finished
-            }//loop for sample1[i] finished
+            for (int i=0; i < sample1.size(); i++) { 
+                arch=='x64' ? mmX64(sample1[i]) : mmX86(sample1[i]) }
             sh "cp ${PROJECTS}/lib/*.so ${PROJECTS}/bin/" //cp libucs_ms.so to TARGET
         break
         default:
