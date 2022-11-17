e2e40449fd9ce401d0ca0f9943a4b8c1401ce7e0
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-17 20:21:39 +0300

    Delete pthUpload.groovy

--- vars/pthUpload.groovy
+++
@@ -1,7 +0,0 @@
-def call (String lvl1, String exe) {
-def nexus_creds = [
-[path: 'secrets/creds/nexus', secretValues: [
-[envVar: 'nexus_pwd', vaultKey: 'password']]]]
-    wrap([$class: 'VaultBuildWrapper', vaultSecrets: nexus_creds]) {
-    sh "./pthUpload.sh ${lvl1} ${exe}" }
-}
