360afcc394ab2423c2a4b31c2dde40e8820d115f
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-18 00:29:55 +0300

    Add files via upload

--- resources/linux/cyasslMake.sh
+++ resources/linux/cyasslMake.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#usage: 
+#usage: cyasslMake.sh | library build for micro_mod.groovt
 chmod 750 configure
 ./configure --enable-opensslextra --enable-aesgcm --enable-sha512 --enable-ripemd --enable-ecc --enable-static
 make 2>errs
