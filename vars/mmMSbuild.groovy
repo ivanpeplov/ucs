ca820d097c34659e40b5c4438870f37703d3ed69
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-20 22:09:21 +0300

    Add files via upload

--- vars/mmMSbuild.groovy
+++ vars/mmMSbuild.groovy
@@ -1,21 +1,21 @@
-def call(String path, String arch) {
+def call(String path, String arch, String rel ) {
     dir (path) {
         switch (path) {
             case ("${WORKSPACE}/cyassl-3.2.0") :
-            bat (script:"msbuild cyassl.sln /t:build /p:configuration=Release64 /p:Platform=${arch}")
+            bat (script:"msbuild cyassl.sln /t:build /p:configuration=Release${rel} /p:Platform=${arch}")
             break
             case ("${WORKSPACE}") : //build for "cyassl, myizip_z, microx_t"
                 loadWinBat('msBuild.bat')
                 sample=mm.split(', ') 
                 for (int i=0; i < sample.size(); i++) {
-                bat (script:"msBuild.bat ${sample[i]} ${arch}") }
+                bat (script:"msBuild.bat ${sample[i]} ${arch} ${rel}") }
             break
             case ("${WORKSPACE}/microx_t/samples") : //build loop for "microp, ucs_mm, ucs_ms, ucs_dt"
                 loadWinBat('msBuild.bat')
                 loadWinBat('msBuild_ucs_mm.bat')
                 sample1=mmm.split(', ') 
                 for (int i=0; i < sample1.size(); i++) {
-                    sample1[i]=='ucs_mm' ? bat(script:"msBuild_ucs_mm.bat ${sample1[i]} ${arch}") : bat(script:"msBuild.bat ${sample1[i]} ${arch}")
+                    sample1[i]=='ucs_mm' ? bat(script:"msBuild_ucs_mm.bat ${sample1[i]} ${arch} ${rel}") : bat(script:"msBuild.bat ${sample1[i]} ${arch} ${rel}")
                     switch (sample1[i]) {
                         case ('microp') :
                         loadWinBat('myizip_lib.bat') //myizip_z.lib service copy for build to ./microx_t/samples/microp
