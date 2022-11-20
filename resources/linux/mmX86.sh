4acc4d0e4d1177e869a84b76f85d7e4806e6579f
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-20 11:34:50 +0300

    Delete mmX86.sh

--- resources/linux/mmX86.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#usage: mX86.sh ${sample}
-pushd $1
-awk '/ATOL Frontol/{ rl = NR + 1 } NR == rl { gsub( /#/,"") } 1' filedefs.inc > tmp && mv tmp filedefs.inc
-echo release 2>errs | xargs -n 1 "${PROJECTS}"/tools/Make
