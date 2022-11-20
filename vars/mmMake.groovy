a393fd769a4c0109b3afb1926cc47fb99d409d4b
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-20 11:35:46 +0300

    Add files via upload

--- vars/mmMake.groovy
+++ vars/mmMake.groovy
@@ -1,25 +1,19 @@
 def call(String path, String arch) {
     dir (path) {
         switch (path) {
-            case ('units/cyassl-3.2.0') :
-            loadLinuxScript('cyasslMake.sh')
-            sh "./cyasslMake.sh" //build for libcyassl.a
-            break
-            case ('units') : //build loop for main modules
-                loadLinuxScript('mmX64.sh') //CCADDFLAGS for X64
-                loadLinuxScript('mmX86.sh') //CCADDFLAGS for X86
-                println "myizip_z, microx_t"
+            case ('units') : //build loop for "cyassl, myizip_z, microx_t"
+                loadLinuxScript('cyasslMake.sh')
+                loadLinuxScript('mmCpp.sh')
                 sample=mm.split(', ') 
-                for (int i=0; i < sample.size(); i++)
-                //elvis groovy operator
-                { arch=='x64' ? sh(script:"./mmX64.sh ${sample[i]}") : sh(script:"./mmX86.sh ${sample[i]}") }
+                for (int i=0; i < sample.size(); i++) {
+                if (sample[i]=='cyassl') {sh "./cyasslMake.sh ${sample[i]}"} //build for libcyassl.a
+                sh(script:"./mmCpp.sh ${sample[i]} ${arch}") }
             break
-            case ('units/microx_t/samples') : //build loop for submodules under microx_t
-                println "microp, ucs_mm, ucs_ms, ucs_dt"
+            case ('units/microx_t/samples') : //build loop for "microp, ucs_mm, ucs_ms, ucs_dt"
+                loadLinuxScript('mmCpp.sh')
                 sample1=mmm.split(', ') 
-                for (int i=0; i < sample1.size(); i++)
-                //elvis groovy operator
-                { arch=='x64' ? sh(script:"../../mmX64.sh ${sample1[i]}") : sh(script:"../../mmX86.sh ${sample1[i]}") }
+                for (int i=0; i < sample1.size(); i++) {
+                sh(script:"./mmCpp.sh ${sample1[i]} ${arch}") }
                 sh "cp ${PROJECTS}/lib/*.so ${PROJECTS}/bin/" //cp libucs_ms.so to TARGET
             break
             default:
