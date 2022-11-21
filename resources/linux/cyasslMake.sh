b857dae4151468d1ac29596a4e5ae7db081ae60d
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-21 03:41:08 +0300

    Delete cyasslMake.sh

--- resources/linux/cyasslMake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#usage: cyasslMake.sh $1| library build for micro_mod.groovt
-pushd $1
-chmod 750 configure
-./configure --enable-opensslextra --enable-aesgcm --enable-sha512 --enable-ripemd --enable-ecc --enable-static
-make 2>errs
-cp ./src/.libs/libcyassl.a ${PROJECTS}/lib
