0cc0b3e7b83c89b0244a30d4c64ed75755c0767d
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-21 03:41:36 +0300

    Add files via upload

--- resources/linux/mmCpp.sh
+++ resources/linux/mmCpp.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 #usage: mX64.sh ${sample} | CCADDFLAGS addin for X64 micro_mod.groovy
+#pushd $1
+if [ "$#" -eq 1 ]; then
+pushd $1
+chmod 750 configure
+./configure --enable-opensslextra --enable-aesgcm --enable-sha512 --enable-ripemd --enable-ecc --enable-static
+make 2>errs
+cp ./src/.libs/libcyassl.a ${PROJECTS}/lib
+else
 pushd $1
 if [ $2 = 'x64' ] ; then
 awk '/^CCFLAGS /{$0=$0 " -DPROCMACH64"}1' filedefs.inc > tmp && mv tmp filedefs.inc
@@ -8,3 +16,5 @@ else
 awk '/ATOL Frontol/{ rl = NR + 1 } NR == rl { gsub( /#/,"") } 1' filedefs.inc > tmp && mv tmp filedefs.inc
 echo release 2>errs | xargs -n 1 "${PROJECTS}"/tools/Make
 fi
+fi
+
