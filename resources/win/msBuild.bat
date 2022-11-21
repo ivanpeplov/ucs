2a9567cac38fea9ca7a5f84829d1e45bc08e710a
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-21 06:22:17 +0300

    Add files via upload

--- resources/win/msBuild.bat
+++ resources/win/msBuild.bat
@@ -1,5 +1,9 @@
-pushd %1
-msbuild %1.sln /t:build /p:configuration=Release%3 /p:Platform=%2
-
-
-
+@echo off
+if not %1==ucs_mm goto other
+if %1==ucs_mm goto mm 
+:other
+cd %1 & msbuild %1.sln /t:build /p:configuration=Release%3 /p:Platform=%2
+goto end
+:mm
+cd %1 & msbuild %1.sln /t:build /p:configuration=REL_UNIEXE_REPOS /p:Platform=%2
+:end
