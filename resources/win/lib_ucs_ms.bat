2c6d5c4e8063bc30263fd76ea010d3d2ee28bab8
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-23 23:41:54 +0300

    Add files via upload

--- resources/win/lib_ucs_ms.bat
+++ resources/win/lib_ucs_ms.bat
@@ -1,4 +1,3 @@
-@echo off
 cd %1
-if %2==x64 ( copy .\x64\REL_DLL\ucs_ms.lib ..\..\samples\test\bin\ )
-if %2==Win32 ( copy .\REL_DLL\ucs_ms.lib ..\..\samples\test\bin\ )
+if %2==x64 ( copy .\Release\micros.lib ..\..\samples\test\bin64\ucs_ms.lib & sed -i s/bin\\ucs_ms.lib/bin64\\ucs_ms.lib/ ..\ucs_dt\ucs_dt.vcxproj)
+if %2==Win32 ( copy .\Release\micros.lib ..\..\samples\test\bin\ucs_ms.lib )
--- resources/win/msBuild.bat
+++ resources/win/msBuild.bat
@@ -2,8 +2,8 @@
 if not %1==ucs_mm goto not_ucs_mm
 if %1==ucs_mm goto ucs_mm 
 :not_ucs_mm
-cd %1 & msbuild %1.sln /t:build /p:configuration=Release%3 /p:Platform=%2
+cd %1 & msbuild %1.sln /t:build /p:configuration=Release /p:Platform=%2
 goto end
 :ucs_mm
-cd %1 & msbuild %1.sln /t:build /p:configuration=REL_UNIEXE_REPOS /p:Platform=%2
+cd %1 & msbuild %1.sln /t:build /p:configuration=Release /p:Platform=%2
 :end
