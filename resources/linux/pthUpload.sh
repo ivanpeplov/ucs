b701f5238c246cd27436cbbe5958993c6958f525
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-26 00:01:38 +0300

    Add files via upload

--- resources/linux/pthUpload.sh
+++ resources/linux/pthUpload.sh
@@ -9,5 +9,5 @@ var1=$(grep -c 'End check with code: 0' ./BIN/CheckSql.log)
 if [ $var -eq $var1 ] ; then echo "All exit codes = 0, $var checks in CheckSql.log" > $2_has_no_err ; fi
 zip -u  $2.zip ktr_xml.log $2_has_no_err
 zip -u -j $2.zip ./BIN/CheckSql.log
-curl -s -u admin:${nexus_pwd} --upload-file $2.zip ${NEXUS_URL}/${SVN_PATH}/$1/
+curl -s -u jenkucs_sa:${nexus_pwd} --upload-file $2.zip ${NEXUS_URL}/${SVN_PATH}/$1/
 rm $2.zip; rm *.log; rm ./BIN/*.log;
--- resources/linux/uploadNexus.sh
+++ resources/linux/uploadNexus.sh
@@ -4,5 +4,5 @@ pushd $1/$2/$3
 if [ -z "$(ls -A )" ]; then echo "Empty"
 else
 zip -r -q $3.zip *
-curl -s -u admin:${nexus_pwd} --upload-file $3.zip ${NEXUS_URL}/${SVN_PATH}/$1/$2/
+curl -s -u jenkucs_sa:${nexus_pwd} --upload-file $3.zip ${NEXUS_URL}/${SVN_PATH}/$1/$2/
 fi
