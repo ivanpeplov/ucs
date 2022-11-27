8e0a8b940cb5ffe28ff9569f2d76ebe2529631e3
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-28 01:16:07 +0300

    Add files via upload

--- resources/linux/pthUpload.sh
+++ resources/linux/pthUpload.sh
@@ -6,8 +6,15 @@ pushd $1
 var=$(grep -c 'End check with code:' ./BIN/CheckSql.log)
 var1=$(grep -c 'End check with code: 0' ./BIN/CheckSql.log)
 #count=%((var - var1))
-if [ $var -eq $var1 ] ; then echo "All exit codes = 0, $var checks in CheckSql.log" > $2_has_no_err ; fi
+if [ $var -eq $var1 ] ; then
+echo "All exit codes = 0, $var checks in CheckSql.log" > $2_has_no_err ;
+fi
+#check file $2_has_no_err exists
+if [ -f "$2_has_no_err" ]; then
 zip -u  $2.zip ktr_xml.log $2_has_no_err
+else
+zip -u  $2.zip ktr_xml.log
+fi
 zip -u -j $2.zip ./BIN/CheckSql.log
 curl -s -u jenkucs_sa:${nexus_pwd} --upload-file $2.zip ${NEXUS_URL}/${SVN_PATH}/$1/
 rm $2.zip; rm *.log; rm ./BIN/*.log;
