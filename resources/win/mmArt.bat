6dcbfa1048b232cd98b131c45d8b6bd9c6658ec3
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-29 23:01:15 +0300

    Add files via upload

--- resources/win/mmArt.bat
+++ resources/win/mmArt.bat
@@ -1,4 +1,4 @@
 @echo off
 cd setup_p & devenv setup_p.sln /build Release
 copy .\Release\setup_p.msi ..\rel_p
-cd ..\rel_p & 7z a setup_p.zip *
+
--- resources/win/mmUpload.bat
+++ resources/win/mmUpload.bat
@@ -1,3 +1,4 @@
 @echo off
+7z a setup_p.zip *
 curl  -s -u jenkucs_sa:%nexus_pwd% --upload-file setup_p.zip  %NEXUS_URL%/%TOOR%/
                 
