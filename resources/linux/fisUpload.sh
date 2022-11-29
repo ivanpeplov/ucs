48ad151523f4b7ef87747ec4a45dd3c4e918ebb3
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-29 20:59:10 +0300

    Add files via upload

---
+++ resources/linux/fisUpload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#usage: ./uploadFIS.sh ${JOB_BASE_NAME} ${SVN} ${NODE_NAME} ${VERSION}
+#usage: ./uploadFIS.sh ${JOB_BASE_NAME} ${ARCH} ${NODE_NAME}
+#check pos. parameters number "$#"
+if [ "$#" -eq 3 ]; then
+zip -r -q $1_$2.zip *
+curl  -s -u jenkucs_sa:${nexus_pwd} --upload-file $1_$2.zip  ${NEXUS_URL}/${TOOR}/$3/               
+else
+zip -r -q $1_$2_$4.zip *
+curl  -s -u jenkucs_sa:${nexus_pwd} --upload-file $1_$2_$4.zip  ${NEXUS_URL}/${TOOR}/$3/               
+fi
