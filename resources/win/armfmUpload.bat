54b140701741ee39ebda18ea7f77b27278e188ca
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-11-30 18:34:48 +0300

    Add files via upload

--- resources/win/armfmUpload.bat
+++ resources/win/armfmUpload.bat
@@ -1,4 +1,4 @@
 @echo off
-7z a %ARCH%_%BUILD_NUMBER%.zip fmUX*, FmUX*
+7z a %ARCH%_%BUILD_NUMBER%.zip fmUX*, FmUX* -x!armfmUpload.bat
 curl  -u jenkucs_sa:%nexus_pwd% --upload-file %ARCH%_%BUILD_NUMBER%.zip  %NEXUS_URL_1%/FM/
                 
--- resources/win/borlandUpload.bat
+++ resources/win/borlandUpload.bat
@@ -1,10 +1,10 @@
 @echo off
 if not "%~3"=="" goto svn
 :svn
-7z a %1_%2.zip *
+7z a %1_%2.zip * -x!borlandUpload.bat -x!getWinSVN.bat
 curl -u jenkucs_sa:%nexus_pwd% --upload-file %1_%2.zip  %NEXUS_URL%/Borland/
 goto end
-7z a %1_%2_%3.zip *
+7z a %1_%2_%3.zip * -x!borlandUpload.bat -x!getWinSVN.bat
 curl -u jenkucs_sa:%nexus_pwd% --upload-file %1_%2_%3.zip  %NEXUS_URL%/Borland/
 :end
                 
