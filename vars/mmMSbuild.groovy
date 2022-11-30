825465fb283eeaabee1bbc11d812ed5879703991
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-01 02:59:40 +0300

    Add files via upload

--- vars/mmMSbuild.groovy
+++ vars/mmMSbuild.groovy
@@ -5,24 +5,17 @@ def call(String path) {
             case ("${WORKSPACE}") : //build loop for "cyassl, myizip_z, microx_t"
                 loadScript(place:'win', name:'mmBuild.bat')
                 sample=mm.split(',')
-                for (int k=0; k < id.size(); k++) {
-                    for (int i=0; i < sample.size(); i++) {
-                        bat (script:"mmBuild.bat ${sample[i]} ${id[k]}")
-                    }
-                }
+                id.each {x->
+                sample.each { y-> bat (script:"mmBuild.bat ${y} ${x}") } }
             break
             case ("microx_t/samples") : //build loop for "microp, ucs_mm, ucs_ms, ucs_dt, ucs_mu"
                 loadScript(place:'win', name:'mmBuild.bat')
                 sample1=mmm.split(',')
-                for (int k=0; k < id.size(); k++) {
-                    for (int i=0; i < sample1.size(); i++) {
-                        bat(script:"mmBuild.bat ${sample1[i]} ${id[k]}")  
-                        if ("${sample1[i]}"=='ucs_ms') {
-                            loadScript(place:'win', name:'lib_ucs_ms.bat') //ucs_ms.lib stub for ucs_ms.dll copy to ./microx_t/sample/test/bin
-                            bat(script:"lib_ucs_ms.bat ${sample1[i]} ${id[k]}" ) 
-                        }
-                    }
-                }
+                id.each { x->
+                sample1.each { y-> bat (script:"mmBuild.bat ${y} ${x}")
+                    if ("${y}"=='ucs_ms') {
+                    loadScript(place:'win', name:'lib_ucs_ms.bat') //ucs_ms.lib stub for ucs_ms.dll copy to ./microx_t/sample/test/bin
+                    bat(script:"lib_ucs_ms.bat ${y} ${x}" ) } } }
                 loadScript(place:'win', name:'mmArt.bat')
                 bat (script:"mmArt.bat") //build for setup_p.zip from setup_p.msi
             break
--- vars/mmMake.groovy
+++ vars/mmMake.groovy
@@ -4,14 +4,13 @@ def call(String path, String arch) {
             case ('units') : //build loop for "cyassl, myizip_z, microx_t"
                 loadScript(place:'linux', name:'mmCpp.sh')
                 sample=mm.split(',') 
-                for (int i=0; i < sample.size(); i++) {
-                sample[i]=='cyassl-3.2.0' ? sh(script:"./mmCpp.sh ${sample[i]}") : sh(script:"./mmCpp.sh ${sample[i]} ${arch}") }  
+                sample.each { x-> x=='cyassl-3.2.0' ? sh(script:"./mmCpp.sh ${x}") : sh(script:"./mmCpp.sh ${x} ${arch}") }
             break
             case ('units/microx_t/samples') : //build loop for "microp, ucs_mm, ucs_ms, ucs_dt"
                 loadScript(place:'linux', name:'mmCpp.sh')
                 loadScript(place:'linux', name:'mmArt.sh')
-                sample1=mmm.split(',') 
-                for (int i=0; i < sample1.size(); i++) { sh(script:"./mmCpp.sh ${sample1[i]} ${arch}") }
+                sample1=mmm.split(',')
+                sample1.each { x-> sh(script:"./mmCpp.sh ${x} ${arch}") }
                 sh "./mmArt.sh" // prepare for upload
             break
         }//switch $path
