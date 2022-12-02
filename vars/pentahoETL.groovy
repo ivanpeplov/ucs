858335d3ee6a69d34587e8e9931be24149d7bec6
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-03 00:35:51 +0300

    Add files via upload

--- vars/pentahoETL.groovy
+++ vars/pentahoETL.groovy
@@ -1,42 +1,30 @@
 import org.apache.commons.io.FilenameUtils
-def call(String path) { //v2.0 01.12.2022
+def call(String path) { //v3.0 03.12.2022
     dir (path) { //path="TestSQLtoNexus"
+        def nexus_creds = [
+            [path: 'secrets/creds/nexus', secretValues: [
+            [envVar: 'nexus_pwd', vaultKey: 'password']]]]
         sh "find . -type d -name .svn -exec rm -rf {} +" //to delete junk /.svn folder recursively at lvl1
         lvl1 = listDir("${path}") //level 1 - group folder [MNR19]
         loadScript(place:'linux', name:'spaceToUnderscore.sh')
         sh "./spaceToUnderscore.sh" //change " " to "_" in filenames recursively
-        loadScript(place:'linux', name:'pthUpload.sh') //bash script for upload .xml to Nexus
+        loadScript(place:'linux', name:'pthUpload_c.sh') //bash script for upload .xml to Nexus
         loadScript(place:'linux', name:'pthConversion.sh') //bash script for PTH conversion
         for (io in lvl1) { lvl2 = listDir("${path}/${io}")
-            lvl2=lvl2 - 'BIN' //[AMSBatch.PTH, BonusETL_top.PTH, ETL_CDWH.PTH]
+            lvl2=lvl2 - 'BIN' //[AMSBatch.PTH, BonusETL_top.PTH] - 'BIN'
             for (jo in lvl2) { ext = FilenameUtils.getExtension(jo)
                 switch (ext) {
                 case ('PTH') :
                     stage=listDir("${path}/${io}/${jo}")
-                    if (stage != '') {
-                        for (lo in stage) {
-                        substage_list = listFiles("${path}/${io}/${jo}/${lo}")
-                        .findAll{it.toLowerCase().contains('.ktr') || it.toLowerCase().contains('.kjb')}
-                            for (mo in substage_list) {
-                            ext  = FilenameUtils.getExtension(mo) //each .ktr/.kjb filename
-                            name = FilenameUtils.removeExtension(mo) //each .ktr/.kjb extension
-                            sh "./pthConversion.sh ${io} ${jo} ${name} ${ext} ${lo}" } //incl. substage (5 parameters)
-                        }
+                    if (stage != '') { //substage conversion
+                        for (lo in stage) { pthConv (r:"${path}", l1:"${io}", l2:"${jo}", ss:"${lo}") } 
                     }
-                        stage_list = listFiles("${path}/${io}/${jo}")
-                        .findAll{it.toLowerCase().contains('.ktr') || it.toLowerCase().contains('.kjb')}
-                        for (ko in stage_list) {
-                            ext  = FilenameUtils.getExtension(ko) 
-                            name = FilenameUtils.removeExtension(ko)
-                            sh "./pthConversion.sh ${io} ${jo} ${name} ${ext}" } //not incl. stage (4 parameters)
-                    def nexus_creds = [
-                    [path: 'secrets/creds/nexus', secretValues: [
-                    [envVar: 'nexus_pwd', vaultKey: 'password']]]]
-                    wrap([$class: 'VaultBuildWrapper', vaultSecrets: nexus_creds]) {
-                    sh "./pthUpload.sh ${io} ${jo}" }
-                    break //PTH stage finished
-                    default: println 'TBD'
-                } //switch .PTH/.ATH/ 
+                        pthConv (r:"${path}", l1:"${io}", l2:"${jo}") //stage conversion
+                    wrap([$class: 'VaultBuildWrapper', vaultSecrets: nexus_creds]) //upload to nexus
+                    { sh "./pthUpload_c.sh ${io} ${jo}" }
+                break //PTH stage finished
+                default: println 'TBD'
+                } //switch EXT finished
             } //loop lvl2 
         } //loop lvl1 [MNR19]
     } //dir ()
