ea92b418b841c2d724a01c140a4f9262af02d67d
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-02 17:02:15 +0300

    Add files via upload

--- src/borland.groovy
+++ src/borland.groovy
@@ -117,19 +117,21 @@ pipeline { //CI-56,58,59
         script {
           switch (LABEL) {
             case ('mms_eod') :
-              makeBorland("${WORKSPACE}", 'mms_eod')
-              makeBorland('C:\\Program Files\\Borland\\CBuilder6\\Bin', 'bin')
+              makeBorland("${WORKSPACE}", "mms_eod")
+              makeBorland("C:\\Program Files\\Borland\\CBuilder6\\Bin", "bin")
             break
             case ('palmera') :
               makeBorland("${WORKSPACE}", 'palmera')
             break
             case ('tid_man') :
-              makeBorland("CARDLIB", 'lib')
-              makeBorland('FORM', 'form')
-              makeBorland("${WORKSPACE}", 'tid_man')
-              makeBorland('C:\\Windows\\System32', '32')
-              makeBorland('FORM\\PRINT.CFG', 'print')
+              makeBorland("CARDLIB", "lib")
+              makeBorland("${WORKSPACE}", "tid_man")
+              makeBorland("C:\\Windows\\System32", "32")
+              makeBorland("FORM\\PRINT.CFG", "print")
+              makeBorland("FORM", "form")
+             
             break
+            default: println "TBD"
           }
         }
       }
---
+++ src/scripts2nexus.groovy
@@ -0,0 +1,46 @@
+@Library("shared-library") _
+pipeline { //CI-57
+    agent {label 'jenkins-rosa'}
+    environment {
+        ROOT="NexusShareAsIs" //project root at SVN
+        SVN_PATH ="${ROOT}" //full path for download fron SVN
+    }
+    stages {
+        stage('SET Env') {
+            steps {
+                script {
+                    setDescription()
+                    setEnv()
+                }
+            }
+        }
+        stage ('PREPARE') {
+            steps {
+                script {
+                    getSVN()
+                }
+            }
+        }
+        stage('GROUP.. UPLOAD') {
+            steps {
+                script {
+                    ktrToNexus("NexusShareAsIs")
+                }
+            }
+        }
+    } //stages
+    post {
+        always {
+            script {             
+                echo 'Clean Workspace'
+                cleanWs()
+            }//script
+        }//always
+        failure {
+            script {
+                echo 'emailing'
+                sendEmail()               
+            }//script
+        }//failure
+    } //post actions
+} //pipeline
