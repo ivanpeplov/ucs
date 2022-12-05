86aa22fbd94e5e2113a6c1e369599b76f0489f8a
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-06 01:57:53 +0300

    Add files via upload

--- vars/prepareFiles.groovy
+++ vars/prepareFiles.groovy
@@ -6,14 +6,10 @@ def call(String name) {
             sh "./prepareFiles.sh"
             break
         case ["borland"] :
-            //bat "xcopy C:\\jenkins\\trunk ${workspace} /i /q /d /e"
-            bat "xcopy ${workspace}\\${SVN}\\${VERSION} ${workspace} /i /q /d /e"
-            bat "mkdir ${TARGET}"
+            bat "xcopy ${workspace}\\${SVN}\\${VERSION} ${workspace} /i /q /d /e & mkdir ${TARGET}"
             break
         case ["mm_win"] :
             bat "xcopy ${workspace}\\MicroModule ${workspace} /i /q /d /e"
-            //bat "copy ${workspace}\\git\\config\\sln_proj.zip ${workspace}"
-            //bat "7z x -y sln_proj.zip" //temp step, extract .sln/.vcxproj for build
         default:
             println ("Default: fm, svn_nexus, etl_nexus")
     }
