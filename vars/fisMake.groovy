3fba7ad69f606f553a27b1c0a7200e89718a3767
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-06 02:28:12 +0300

    Add files via upload

--- vars/fisMake.groovy
+++ vars/fisMake.groovy
@@ -1,7 +1,6 @@
 def call(String operation) {
     dir (operation) {
-    //List after ACTIVE CHOICE properties is string divided ','. Need a split() to list []
     units = MODULES.split(',').toList()
-    units.each {f -> sh "pushd ${f} ; echo ${CLEAR} ${RELEASE} | xargs -n 1 ${PROJECTS}/tools/Make"}
+    units.each {f -> sh "cd ${f} ; echo ${CLEAR} ${RELEASE} | xargs -n 1 Make"}
     }
 }
--- vars/pthConversion.groovy
+++ vars/pthConversion.groovy
@@ -1,7 +1,7 @@
 import org.apache.commons.io.FilenameUtils
 def call (Map pth=[:]) { //pth conversion - .ktr/.kjb Pentaho files
     pth.get('ss', '') // default value for map. change 'null' to ''
-    list = listFiles("${pth.r}/${pth.l1}/${pth.l2}/${pth.ss}").findAll { it=~ /(?i)\.(?:ktr|kjb)$/ }
+    list = listFiles ("${pth.r}/${pth.l1}/${pth.l2}/${pth.ss}").findAll { it=~ /(?i)\.(?:ktr|kjb)$/ }
     for (i in list) {
     ext  = FilenameUtils.getExtension(i) //each .ktr/.kjb filename
     name = FilenameUtils.removeExtension(i) //each .ktr/.kjb extension
