75ffba1046bc351d5036725f0ab0452b38eb1d58
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-06 00:37:15 +0300

    Add files via upload

--- vars/makeBorland.groovy
+++ vars/makeBorland.groovy
@@ -1,34 +1,20 @@
 def call(String rt, String label) {
     dir (rt) {
         switch (label) {  //module selection
-        case ("palmerauloade") : //PalmeraUloader building
-        makePalmMms("${label}", "${bmp}")
-        break
-        case ("mmseod") : //mmsEOD building
-        makePalmMms("${label}", "${bmp}") 
-        break
-        case ("bin") : 
-        bpl.split(',').each { f -> bat "xcopy ${f} ${TARGET}" }
-        break
-        case ("cardlib") : //TID Manager building
-        bat "make -f cardlib.mak & xcopy C:\\bpl\\*.bpl ${TARGET}"
-        break
-        case ("cardpro") :
-        bat "make -f cardpro.mak & xcopy cardpro.exe ${TARGET} & xcopy cardpro.ini ${TARGET}"
-        break
-        case ("32") : 
-        bpl.split(',').each { f -> bat "xcopy ${f} ${TARGET}" }
-        break
+        //PalmeraUloader building
+        case ("palmerauloade") : makePalmMms("${label}", "${bmp}"); break; 
+        //mmsEOD building
+        case ("mmseod") : makePalmMms("${label}", "${bmp}"); break;
+        case ("bin") : bpl.split(',').each { f -> bat "xcopy ${f} ${TARGET}" }; break;
+        //TID Manager building
+        case ("cardlib") : bat "make -f cardlib.mak & xcopy C:\\bpl\\*.bpl ${TARGET}"; break; 
+        case ("32") : bpl.split(',').each { f -> bat "xcopy ${f} ${TARGET}" }; break;
+        case ("cardpro") : bat "make -f cardpro.mak & xcopy cardpro.exe ${TARGET} & xcopy cardpro.ini ${TARGET}" ;  break;
         case ("form") :
             dir = listDirWin("${WORKSPACE}\\${rt}") - 'TEMPLATE'
             for (io in dir) {
-                switch (io) {
-                case ('PRINT.CFG') :
-                bmp.split(',').each {f->bat "xcopy .\\${io}\\${f} ${TARGET}\\${rt}\\${io}\\"}
-                break
-                default :
-                bat "cd ${io} & make -f ${io}.mak"
-                bat "cd ${io} & xcopy ${io}.dll ${TARGET}\\${rt}\\${io}\\" } }      
+                if (io=='PRINT.CFG') { bmp.split(',').each {f->bat "xcopy .\\${io}\\${f} ${TARGET}\\${rt}\\${io}\\"} }
+                else { bat "cd ${io} & make -f ${io}.mak & xcopy ${io}.dll ${TARGET}\\${rt}\\${io}\\" } }
         break
         }
     }
