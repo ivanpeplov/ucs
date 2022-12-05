f09223387d143f410c629ec0bf8df0c67a7a23b9
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-05 23:51:39 +0300

    Add files via upload

--- vars/fisMake.groovy
+++ vars/fisMake.groovy
@@ -1,7 +1,7 @@
 def call(String operation) {
     dir (operation) {
     //List after ACTIVE CHOICE properties is string divided ','. Need a split() to list []
-    unitList = MODULES.split(',').toList()
-    unitList.each {filename -> sh "pushd ${filename} ; echo ${CLEAR} ${RELEASE} | xargs -n 1 ${PROJECTS}/tools/Make"}
+    units = MODULES.split(',').toList()
+    units.each {f -> sh "pushd ${f} ; echo ${CLEAR} ${RELEASE} | xargs -n 1 ${PROJECTS}/tools/Make"}
     }
 }
