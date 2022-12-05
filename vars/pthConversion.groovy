2416d8ae20ca67b606de78d0ae31214ca0b93e9c
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-06 02:01:04 +0300

    Add files via upload

--- vars/pthConversion.groovy
+++ vars/pthConversion.groovy
@@ -2,7 +2,6 @@ import org.apache.commons.io.FilenameUtils
 def call (Map pth=[:]) { //pth conversion - .ktr/.kjb Pentaho files
     pth.get('ss', '') // default value for map. change 'null' to ''
     list = listFiles("${pth.r}/${pth.l1}/${pth.l2}/${pth.ss}").findAll { it=~ /(?i)\.(?:ktr|kjb)$/ }
-    //.findAll{it.toLowerCase().contains('.ktr') || it.toLowerCase().contains('.kjb')}
     for (i in list) {
     ext  = FilenameUtils.getExtension(i) //each .ktr/.kjb filename
     name = FilenameUtils.removeExtension(i) //each .ktr/.kjb extension
