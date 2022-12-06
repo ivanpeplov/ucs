2c5029dec9e09e4dd6305b41aad3096e179edc72
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-06 13:14:16 +0300

    Delete fisMake.groovy

--- vars/fisMake.groovy
+++
@@ -1,6 +0,0 @@
-def call(String operation) {
-    dir (operation) {
-    units = MODULES.split(',').toList()
-    units.each {f -> sh "cd ${f} ; echo ${CLEAR} ${RELEASE} | xargs -n 1 Make"}
-    }
-}
