08ec564652abf7b2e156d46ef29bbb200ea73ce8
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-06 13:26:49 +0300

    Delete hsmSign.groovy

--- vars/hsmSign.groovy
+++
@@ -1,7 +0,0 @@
-//FM sign process
-def call(String folder, String hsm) {
-    dir (folder) {
-        if (hsm=='ppcfm') {sh "mkfm -k ABG/fm -ffmUX.bin -oFmUX.fm <<< ${SIGN}"}
-        else {bat "echo ${SIGN} | mkfm -k ABG/fm -ffmUX -ofmUX.fm"}
-    }
-}
