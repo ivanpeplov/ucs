6d214001e6c6b4f821e3505cec94b5ac0319f923
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-07 19:05:57 +0300

    Add files via upload

---
+++ resources/win/getSVN.bat
@@ -0,0 +1,3 @@
+@echo off
+svn co --quiet --username jenkins --password=%svn_pwd% --non-interactive --trust-server-cert-failures=unknown-ca,cn-mismatch,expired,not-yet-valid,other https://%SVN_URL%/%SVN_PATH%
+        
