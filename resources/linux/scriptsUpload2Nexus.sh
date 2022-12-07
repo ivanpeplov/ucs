5837f102bf368c59e6e87b64a6f9ddbfedd16a2d
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-07 18:55:59 +0300

    Delete scriptsUpload2Nexus.sh

--- resources/linux/scriptsUpload2Nexus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#usage: scriptsUpload2Nexus.sh ${lvl1} ${lvl2} ${lvl3}
-pushd $1/$2/$3
-if [ -z "$(ls -A )" ]; then echo "Empty"
-else
-zip -r $3.zip *
-curl -u jenkucs_sa:${nexus_pwd} --upload-file $3.zip ${NEXUS_URL}/${SVN_PATH}/$1/$2/
-fi
