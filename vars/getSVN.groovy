34a738701efcfc470dedc61e5787d850398bce05
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-07 19:06:27 +0300

    Add files via upload

--- vars/getSVN.groovy
+++ vars/getSVN.groovy
@@ -1,17 +1,17 @@
 def call() {
 //download /units folder from SVN
-def osName = isUnix() ? "UNIX":"WINDOWS"
+def osName = isUnix() ? "UNIX":"WINDOWS" // calling elvis
 echo "osName: " + osName
     def svn_creds = [
     [path: 'secrets/creds/svn', secretValues: [
     [envVar: 'svn_pwd', vaultKey: 'password']]]]
     wrap([$class: 'VaultBuildWrapper', vaultSecrets: svn_creds]) {
         if(isUnix()) {
-          loadScript(place:'linux', name:'getNixSVN.sh')
-          sh "./getNixSVN.sh"
+          loadScript(place:'linux', name:'getSVN.sh')
+          sh "./getSVN.sh"
         } else {
-          loadScript(place:'win', name:'getWinSVN.bat')
-          bat "getWinSVN.bat"
+          loadScript(place:'win', name:'getSVN.bat')
+          bat "getSVN.bat"
         }
     }
 }
