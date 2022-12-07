8779ea30f6259170d3fa2296cb204ae7a941e1e6
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-07 19:47:54 +0300

    Delete conversionPTH.groovy

--- vars/conversionPTH.groovy
+++
@@ -1,9 +0,0 @@
-import org.apache.commons.io.FilenameUtils
-def call (Map pth=[:]) { //pth conversion - .ktr/.kjb Pentaho files
-    pth.get('ss', '') // default value for map. change 'null' to ''
-    list = listFiles ("${pth.r}/${pth.l1}/${pth.l2}/${pth.ss}").findAll { it=~ /(?i)\.(?:ktr|kjb)$/ } //get all .ktr/.kjb
-    for (i in list) {
-    ext  = FilenameUtils.getExtension(i) //each .ktr/.kjb filename
-    name = FilenameUtils.removeExtension(i) //each .ktr/.kjb extension
-    sh "./conversionPTH.sh ${pth.l1} ${pth.l2} ${name} ${ext} ${pth.ss}" } //incl. substage (5 parameters)
-}
