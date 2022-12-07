511d71f5fe578e8e6824a940cb278adf8805bebc
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-07 19:05:37 +0300

    Delete getWinSVN.bat

--- resources/win/getWinSVN.bat
+++
@@ -1,2 +0,0 @@
-svn co --quiet --username jenkins --password=%svn_pwd% --non-interactive --trust-server-cert-failures=unknown-ca,cn-mismatch,expired,not-yet-valid,other https://%SVN_URL%/%SVN_PATH%
-        
