8d2d6491af7c08e1545a1e98cf2c867b9f75ec3e
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-13 01:00:37 +0300

    Add files via upload

--- vars/loadScript.groovy
+++ vars/loadScript.groovy
@@ -1,5 +1,6 @@
 def call(Map script=[:]) { 
   def scriptcontents = libraryResource "${script.place}/${script.name}"    
   writeFile file: "${script.name}", text: scriptcontents 
-  if ("${script.place}"=='linux') {sh "chmod a+x ./${script.name}"}
+  if (script.place=='linux') {sh "chmod a+x ./${script.name}"}
+  if (script.place=='gradle') {sh "mv ${script.name} build.gradle"}
 }
