ba3a3918eafc14b7ec456df28b7106222b845227
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-13 01:44:55 +0300

    Delete xidelMavenMetadata.sh

--- resources/linux/xidelMavenMetadata.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-xquery='''
-for $x in doc("maven-metadata.xml")/metadata/versioning/versions
-return $x/version'''
-/var/lib/jenkins/xidel http://10.255.252.161:8081/repository/ucs_repo/ru/ucs/mmcore/maven-metadata.xml --xquery  "$xquery" 
