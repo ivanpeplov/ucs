60ac15a9a9adf34548c12c264f6f442512132834
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-13 13:03:09 +0300

    Add files via upload

--- vars/setDescription.groovy
+++ vars/setDescription.groovy
@@ -4,6 +4,9 @@ def call() {
     case ["fis", "borland"]:
     currentBuild.description = "node: ${NODE_NAME}\n label: ${LABEL}; svn: ${SVN}; version: ${VERSION}\nbuild_number: ${BUILD_NUMBER}"
     break
+    case ["mm_java"]:
+    currentBuild.description = "node: ${NODE_NAME}\n label: ${LABEL}; build_number: ${BUILD_NUMBER}"
+    break
     default:
     currentBuild.description = "node: ${NODE_NAME}\nbuild_number: ${BUILD_NUMBER}"
     }
