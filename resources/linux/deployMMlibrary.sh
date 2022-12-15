b4486469b8cee69d738427da34044d0c3f0cdbb9
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-15 15:11:54 +0300

    Add files via upload

--- resources/linux/deployMMlibrary.sh
+++ resources/linux/deployMMlibrary.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gradle -DARG=${VERSION} downloadFile -b tools_lib.gradle
 touch local.properties & echo 'sdk.dir = /home/jenkins/android' >> local.properties
-gradle build -b build_lib
+gradle build -b build_lib.gradle
 gradle publish -b tools_lib.gradle
