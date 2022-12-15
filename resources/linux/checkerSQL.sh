9051a662f4609735eba52ccd51465e3eaa31bf89
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-15 22:15:57 +0300

    Add files via upload

---
+++ resources/linux/checkerSQL.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#usage: ConversionPTH.sh ${lvl1} ${exe} ${name} ${ext} ${stage}
+set +e
+pushd ./$1/BIN; echo "---------- $3.xml ----------" >> CheckSql.log;
+if [  "$#" -eq 4 ]; then
+  java -jar checkersql.jar "../$2/$3.xml"
+  cd ..; zip  -u $2.zip ./$2/$3.xml
+else
+  java -jar checkersql.jar "../$2/$5/$3.xml"
+  cd ..; zip  -u $2.zip ./$2/$5/$3.xml
+fi
+# true - for testing only. In prod - comment it
---
+++ resources/linux/xsltcPTH.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#usage: ConversionPTH.sh ${lvl1} ${exe} ${name} ${ext} ${stage}
+set +e
+#check pos. parameters number "$#"
+if [ "$#" -eq 4 ]; then
+pushd $1; java -jar BIN/xsltc.jar -i "$2/$3.$4" -o "$2/$3.xml" -l stdout.log -x BIN/pth2lst.xslt
+else
+pushd $1; java -jar BIN/xsltc.jar -i "$2/$5/$3.$4" -o "$2/$5/$3.xml" -l stdout.log -x BIN/pth2lst.xslt
+fi
+cat stdout.log >> ktr_xml.log
+# true - for testing only. In prod - comment it
