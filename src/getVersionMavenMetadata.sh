9012a8ed2c712d3a8965ff80b4d6b36afcf20ba0
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-15 15:19:33 +0300

    Add files via upload

---
+++ src/getVersionMavenMetadata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+xquery='''
+for $x in doc("maven-metadata.xml")/metadata/versioning/versions
+return $x/version'''
+/var/lib/jenkins/bin/xidel $NEXUS_MAVEN_ORPO/ru/ucscards/mmcore/maven-metadata.xml --xquery  "$xquery"
