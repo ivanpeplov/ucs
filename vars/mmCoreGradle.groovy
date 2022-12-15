9aea2cd0dc368d8914370bbc27221e44d692b8e9
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-15 13:52:04 +0300

    Add files via upload

--- vars/mmCoreGradle.groovy
+++ vars/mmCoreGradle.groovy
@@ -4,6 +4,6 @@ def call() {
     loadScript(place:'linux', name:'getVersionFromSvnPom.sh')
     output = sh returnStdout: true, script: "./getVersionFromSvnPom.sh"
     versionFromPom = output.trim()
-    sh "gradle -Pversion=${versionFromPom} build"
-    sh "gradle -Pversion=${versionFromPom} publish"
+    sh "gradle -Pversion=${versionFromPom} build -b build_core.gradle"
+    sh "gradle -Pversion=${versionFromPom} publish -b build_core.gradle"
 }
