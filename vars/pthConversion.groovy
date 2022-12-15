ce3983217caf275b6f1f3d435603685e8b9cb7be
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-15 22:14:54 +0300

    Add files via upload

--- vars/pthConversion.groovy
+++ vars/pthConversion.groovy
@@ -1,9 +1,13 @@
 import org.apache.commons.io.FilenameUtils
 def call (Map pth=[:]) { //pth conversion - .ktr/.kjb Pentaho files
     pth.get('ss', '') // default value for map. change 'null' to ''
-    list = listFiles ("${pth.r}/${pth.l1}/${pth.l2}/${pth.ss}").findAll { it=~ /(?i)\.(?:ktr|kjb)$/ } //get all .ktr/.kjb
+    if (pth.ex=='PTH') 
+    {list = listFiles ("${pth.r}/${pth.l1}/${pth.l2}/${pth.ss}").findAll { it=~ /(?i)\.(?:ktr|kjb)$/ } }
+    else {list = listFiles ("${pth.r}/${pth.l1}/${pth.l2}/${pth.ss}").findAll { it=~ /(?i)\.(?:xml)$/ } }//get all .ktr/.kjb
     for (i in list) {
-    ext  = FilenameUtils.getExtension(i) //each .ktr/.kjb filename
-    name = FilenameUtils.removeExtension(i) //each .ktr/.kjb extension
-    sh "./pthConversion.sh ${pth.l1} ${pth.l2} ${name} ${ext} ${pth.ss}" } //incl. substage (5 parameters)
+        ext  = FilenameUtils.getExtension(i) //each .ktr/.kjb filename
+        name = FilenameUtils.removeExtension(i) //each .ktr/.kjb extension
+        if (pth.ex=='PTH') {sh "./xsltcPTH.sh ${pth.l1} ${pth.l2} ${name} ${ext} ${pth.ss}"}
+        sh "./checkerSQL.sh ${pth.l1} ${pth.l2} ${name} ${ext} ${pth.ss}"
+    }
 }
