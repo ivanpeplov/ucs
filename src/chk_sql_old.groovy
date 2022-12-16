8fe78f973b880165ea4976e7b267d18586d5f6c6
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-16 22:08:21 +0300

    Add files via upload

--- src/chk_sql_old.groovy
+++ src/chk_sql_old.groovy
@@ -1,33 +1,8 @@
 @Library("shared-library") _
-properties([
-  parameters([
-    [$class: 'CascadeChoiceParameter', 
-      choiceType: 'PT_SINGLE_SELECT', 
-      description: 'Select TIETO release',
-      name: 'MNR', 
-      script: [
-        $class: 'GroovyScript', 
-        script: [
-          classpath: [], 
-          sandbox: false, 
-          script: '''
-            svn_url='172.16.10.230/scm/svn/dev'
-            proj='TestSQLtoNexus'
-            proc1= ["bash", "-c", "svn list --username jenkins --password mRovmZVpt  https://${svn_url}/${proj}"].execute()
-            proc2= ["bash", "-c", "rev | cut -c2- | rev"].execute()
-            all = proc1 | proc2
-            choices = all.text
-            return choices.split().toList()
-            '''
-        ]
-      ]
-    ],
-  ])
-])
 pipeline { //CI-60/CI-67
     agent {label 'jenkins-rosa'}
     environment {
-        ROOT="TestSQLtoNexus/${MNR}" //project root at SVN 
+        ROOT="TestSQLtoNexus" //project root at SVN 
         SVN_PATH ="${ROOT}" //full path for download fron SVN
     }
     stages {
@@ -50,7 +25,7 @@ pipeline { //CI-60/CI-67
             steps {
                 script {
                     println "ETL cycle"
-                    pthScript("${MNR}")
+                    pthScript_old("TestSQLtoNexus")
                 }
             }
         }
