f8ca8f8bfeddd43828e249c4cc3daf6fecf74e42
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-16 03:08:55 +0300

    Add files via upload

--- vars/pthConversion.groovy
+++ vars/pthConversion.groovy
@@ -1,13 +1,13 @@
 import org.apache.commons.io.FilenameUtils
 def call (Map pth=[:]) { //pth conversion - .ktr/.kjb Pentaho files
     pth.get('ss', '') // default value for map. change 'null' to ''
-    if (pth.ex=='PTH') 
-    {list = listFiles ("${pth.r}/${pth.l1}/${pth.l2}/${pth.ss}").findAll { it=~ /(?i)\.(?:ktr|kjb)$/ } }
-    else {list = listFiles ("${pth.r}/${pth.l1}/${pth.l2}/${pth.ss}").findAll { it=~ /(?i)\.(?:xml)$/ } }//get all .ktr/.kjb
+    if (pth.tex=='PTH') //filter .ktr/.kjb or .xml files
+    { list = listFiles ("${pth.r}/${pth.l1}/${pth.l2}/${pth.ss}").findAll { it=~ /(?i)\.(?:ktr|kjb)$/ } } //get all .ktr/.kjb
+    else { list = listFiles ("${pth.r}/${pth.l1}/${pth.l2}/${pth.ss}").findAll { it=~ /(?i)\.(?:xml)$/ } } //get all .xml
     for (i in list) {
-        ext  = FilenameUtils.getExtension(i) //each .ktr/.kjb filename
-        name = FilenameUtils.removeExtension(i) //each .ktr/.kjb extension
-        if (pth.ex=='PTH') {sh "./xsltcPTH.sh ${pth.l1} ${pth.l2} ${name} ${ext} ${pth.ss}"}
+        ext  = FilenameUtils.getExtension(i) //each filename
+        name = FilenameUtils.removeExtension(i) //each extension
+        if (pth.tex=='PTH') { sh "./xsltcPTH.sh ${pth.l1} ${pth.l2} ${name} ${ext} ${pth.ss}" }
         sh "./checkerSQL.sh ${pth.l1} ${pth.l2} ${name} ${ext} ${pth.ss}"
     }
 }
--- vars/pthScript.groovy
+++ vars/pthScript.groovy
@@ -1,27 +1,27 @@
 import org.apache.commons.io.FilenameUtils
-def call(String path) { //v3.0 03.12.2022
+def call(String path) { //v4.0 15.12.2022
     dir (path) { //path="TestSQLtoNexus"
-        def nexus_creds = [
+        def nexus_creds = [ //masking nexus credentials
             [ path: 'secrets/creds/nexus', secretValues: [
             [ envVar: 'nexus_pwd', vaultKey: 'password']]]]
         sh "find . -type d -name .svn -exec rm -rf {} +" //to delete junk /.svn folder recursively at lvl1
         loadScript(place:'linux', name:'spaceToUnderscore.sh')
         sh "./spaceToUnderscore.sh" //change " " to "_" in filenames recursively
         loadScript(place:'linux', name:'pthUpload.sh') //bash script for upload .xml to Nexus
-        loadScript(place:'linux', name:'checkerSQL.sh')
-        loadScript(place:'linux', name:'xsltcPTH.sh')
-        lvl1 = listDir("${path}") //level 1 - group folder [MNR19]
+        loadScript(place:'linux', name:'checkerSQL.sh') //sql checker
+        loadScript(place:'linux', name:'xsltcPTH.sh') // ktr2xml converter
+        lvl1 = listDir("${path}") //level 1 - release folder [MNR19]
         for (io in lvl1) { lvl2 = listDir("${path}/${io}")
-            lvl2=lvl2 - 'BIN' //[AMSBatch.PTH, BonusETL_top.PTH, NTPREFS.XDB] - 'BIN'
+            lvl2=lvl2 - 'BIN' //[AMSBatch.PTH, BonusETL.PTH, ..., NTPREFS.XDB] - 'BIN'
             for (jo in lvl2) {
-              ext = FilenameUtils.getExtension(jo)
+              ext = FilenameUtils.getExtension(jo) // [PTH, PTH, ..., XDB]
               stage=listDir("${path}/${io}/${jo}") 
-              if (stage != '') { for (lo in stage) { 
-              pthConversion(ex:"${ext}", r:"${path}", l1:"${io}", l2:"${jo}", ss:"${lo}") } } /*substage*/
+              if (stage != '') { for (lo in stage) { //only files or has folders
+              pthConversion(tex:"${ext}", r:"${path}", l1:"${io}", l2:"${jo}", ss:"${lo}") } } /*substage*/
               //stage conversion
-              pthConversion(ex:"${ext}", r:"${path}", l1:"${io}", l2:"${jo}") /*stage*/ 
-              wrap([$class: 'VaultBuildWrapper', vaultSecrets: nexus_creds]) //upload to nexus
-              { sh "./pthUpload.sh ${io} ${jo}" }
+              pthConversion(tex:"${ext}", r:"${path}", l1:"${io}", l2:"${jo}") /*stage*/ 
+              wrap([$class: 'VaultBuildWrapper', vaultSecrets: nexus_creds]) 
+              { sh "./pthUpload.sh ${io} ${jo}" } //upload to nexus
             } //loop lvl2 
         } //loop lvl1
     } //dir ()
