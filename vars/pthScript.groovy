4d8b774d2ce41bce951d066509eaf12755846ec6
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2022-12-18 16:08:37 +0300

    Add files via upload

--- vars/pthScript.groovy
+++ vars/pthScript.groovy
@@ -1,23 +1,20 @@
-import org.apache.commons.io.FilenameUtils
 def call(String path) { //v4.0 15.12.2022
     dir (path) { //path="TestSQLtoNexus"
         def nexus_creds = [ //masking nexus credentials
             [ path: 'secrets/creds/nexus', secretValues: [
             [ envVar: 'nexus_pwd', vaultKey: 'password']]]]
-        sh "find . -type d -name .svn -exec rm -rf {} +" //to delete junk /.svn folder recursively at lvl1
         loadScript(place:'linux', name:'spaceToUnderscore.sh')
-        sh "./spaceToUnderscore.sh" //change " " to "_" in filenames recursively
+        sh """./spaceToUnderscore.sh; find . -type d -name .svn -exec rm -rf {} + """
         loadScript(place:'linux', name:'pthUpload.sh') // bash
         loadScript(place:'linux', name:'pthConversion.sh') // ktr2xml + checkerSQL
         loadScript(place:'linux', name:'checkerSQL.sh') // sql checker
-        //loadScript(place:'linux', name:'xsltcPTH.sh') // ktr2xml converter
         lvl2 = listDir("${path}") - 'BIN' //[AMSBatch.PTH, BonusETL.PTH, ..., NTPREFS.XDB] - 'BIN'
         for (jo in lvl2) {
-            ext = FilenameUtils.getExtension(jo) // [PTH, PTH, ..., XDB]
+            ext = jo.substring(jo.indexOf(".")+1); // [PTH, PTH, ..., XDB]
             stage = listDir("${path}/${jo}") 
-            if (stage != '') { for (lo in stage) { // if has folders - recursively process them
-            pthConversion (todo:"${ext}", l1:"${path}", l2:"${jo}", ss:"${lo}") } } /*substage recursion*/
-            //stage conversion
+            if (stage != '') { for (lo in stage) // if has folders - recursively process them
+            { pthConversion (todo:"${ext}", l1:"${path}", l2:"${jo}", ss:"${lo}") } /*substage recursion*/
+            } //stage conversion
             pthConversion (todo:"${ext}", l1:"${path}", l2:"${jo}") /*stage no recursion*/ 
             wrap([$class: 'VaultBuildWrapper', vaultSecrets: nexus_creds]) 
             { sh "./pthUpload.sh ${jo}" } // upload to nexus
