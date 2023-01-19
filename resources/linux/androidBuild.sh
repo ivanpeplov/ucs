32035471396083436b42145f7aadd7021959d1ca
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-01-19 13:43:07 +0300

    Add files via upload

--- resources/linux/androidBuild.sh
+++ resources/linux/androidBuild.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-# mm_java, mm_android
+#mm_android
 touch local.properties & echo 'sdk.dir = /home/jenkins/tools/android' >> local.properties
-if [ "${LABEL}" = "mmcore" ]; then
-#delete last line
-sed -i '$d' pom.xml
-#attached after last line
-cat addToPom.xml >> pom.xml
-mvn deploy
-fi
-if [ "${LABEL}" = "mmlibrary" ]; then
-gradle -DARG=${VERSION} downloadFile -b tools_lib.gradle
-gradle -DMINSDK=${MINSDK} build -b build_lib.gradle
-gradle -DMINSDK=${MINSDK} publish -b tools_lib.gradle
-fi
-if [ "${LABEL}" = "sample" ]; then
-gradle build -b sample.gradle
-fi
-if [ "${LABEL}" = "evotor" ]; then
-gradle downloadFile -b evotor.gradle
-cp evotor_app.gradle ./app/build.gradle 
-cd app; gradle build -b build.gradle
-fi
+case "${LABEL}" in
+mmcore) sed -i '$d' pom.xml
+        #attached after last line
+        cat addToPom.xml >> pom.xml
+        mvn deploy
+        ;;
+mmlibrary) gradle -DARG=${VERSION} downloadFile -b tools_lib.gradle
+           gradle -DMINSDK=${MINSDK} build -b build_lib.gradle
+           gradle -DMINSDK=${MINSDK} publish -b tools_lib.gradle
+           ;;
+sample) gradle build -b sample.gradle
+        ;;
+evotor) gradle downloadFile -b evotor.gradle
+        cp evotor_app.gradle ./app/build.gradle 
+        cd app; gradle build -b build.gradle
+        ;;
+esac
+
