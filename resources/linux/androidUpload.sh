bae2683010b9c93ac705bb4891e1e2b901695992
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-01-19 23:29:15 +0300

    Add files via upload

--- resources/linux/androidUpload.sh
+++ resources/linux/androidUpload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-zip -r -q $1.zip * -x evotorUpload.sh
+zip -r -q $1.zip * -x androidUpload.sh
 curl  -s -u jenkucs_sa:${nexus_pwd} --upload-file $1.zip  ${NEXUS_URL}/MicroModule/Android/               
