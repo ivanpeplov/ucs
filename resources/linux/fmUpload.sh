17007782f7da7bb527e7673bf568017e6cf79629
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-01-19 23:25:54 +0300

    Add files via upload

--- resources/linux/fmUpload.sh
+++ resources/linux/fmUpload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # fm build
-zip  ${ARCH}.zip [f-F]mUX.* -x ppcfmUpload.sh
+zip  ${ARCH}.zip [f-F]mUX.* -x fmUpload.sh
 curl -u jenkucs_sa:${nexus_pwd} --upload-file ${ARCH}.zip  ${NEXUS_URL_1}/FM/
--- resources/linux/mmUpload.sh
+++ resources/linux/mmUpload.sh
@@ -5,9 +5,9 @@
 #usage: ./uploadFIS.sh ${JOB_BASE_NAME} ${ARCH} ${NODE_NAME}
 #check pos. parameters number "$#"
 if [ "$#" -eq 3 ]; then
-zip -r -q $1_$2.zip * -x fisUpload.sh
+zip -r -q $1_$2.zip * -x mmUpload.sh
 curl  -s -u jenkucs_sa:${nexus_pwd} --upload-file $1_$2.zip  ${NEXUS_URL}/${TOOR}/$3/               
 else
-zip -r -q $1_$2_$4.zip * -x fisUpload.sh
+zip -r -q $1_$2_$4.zip * -x mmUpload.sh
 curl  -s -u jenkucs_sa:${nexus_pwd} --upload-file $1_$2_$4.zip  ${NEXUS_URL}/${TOOR}/$3/               
 fi
