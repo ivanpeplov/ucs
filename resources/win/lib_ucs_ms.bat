c48b820c0dea460e2695b53fa54bc7791bc116ef
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-01-20 17:02:45 +0300

    Delete lib_ucs_ms.bat

--- resources/win/lib_ucs_ms.bat
+++
@@ -1,3 +0,0 @@
-cd %1
-if %2==x64 ( copy .\Release\micros.lib ..\..\samples\test\bin64\ucs_ms.lib & sed -i s/bin\\ucs_ms.lib/bin64\\ucs_ms.lib/ ..\ucs_dt\ucs_dt.vcxproj)
-if %2==Win32 ( copy .\Release\micros.lib ..\..\samples\test\bin\ucs_ms.lib )
