e232e771d315e828b86436fc75f69d9395684c40
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-01-20 16:44:27 +0300

    Add files via upload

---
+++ resources/linux/pthChecker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#                          $1      $2     $3      $4   
+#usage: pthChecker.sh   ${lvl2} ${name} ${ext} ${stage}
+set +e
+if [ "$#" -eq 3 ]; then
+java -jar BIN/xsltc.jar -i "$1/$2.$3" -o "$1/$2.$3.xml" -l stdout.log -x BIN/pth2lst.xslt
+else
+java -jar BIN/xsltc.jar -i "$1/$4/$2.$3" -o "$1/$4/$2.$3.xml" -l stdout.log -x BIN/pth2lst.xslt
+fi
+cat stdout.log >> ktr_xml.log
+pushd BIN; echo "---------- $2.$3.xml ----------" >> CheckSql.log;
+if [  "$#" -eq 3 ]; then
+  java -jar checkersql.jar "../$1/$2.$3.xml"
+  popd +0; zip  -u $1.zip ./$1/$2.$3.xml
+else
+  java -jar checkersql.jar "../$1/$4/$2.$3.xml"
+  popd +0; zip  -u $1.zip ./$1/$4/$2.$3.xml
+fi
+# true - for testing only. In prod - comment it
