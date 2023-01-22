440556fff232565fb12f7e2060a8d6520575dcb1
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-01-23 01:08:34 +0300

    Add files via upload

--- vars/mmWin.groovy
+++ vars/mmWin.groovy
@@ -1,8 +1,8 @@
 def call (String yml, String arch) {
-    id=arch.split(',')
     smpl=yml.split(',')
+    id=arch.split(',')
     id.each { x-> smpl.each { y-> bat (script:"mmBuild.bat ${y} ${x}" ) 
-    if (y=='ucs_ms') {
-        loadScript(place:'win', name:'mmMicrosCopy.bat') 
-        bat(script:"mmMicrosCopy.bat ${y} ${x}" ) } } }
+        if (y=='ucs_ms') {
+            loadScript(place:'win', name:'mmLib.bat') 
+            bat(script:"mmLib.bat ${y} ${x}" )  } } }
 }
