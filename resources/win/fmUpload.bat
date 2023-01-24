e2ca542608f7aa0d4c8058744951f39168097f3e
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-01-25 00:00:24 +0300

    Add files via upload

--- resources/win/fmUpload.bat
+++ resources/win/fmUpload.bat
@@ -2,12 +2,17 @@
 rem fm
 if %LABEL%==armfm goto arm_fm
 if %LABEL%==pseutils goto pseutils
+if %LABEL%==fmman goto fmman
 :arm_fm 
 7z a %LABEL%.zip fmUX*, FmUX* -x!fmUpload.bat
 curl  -u jenkucs_sa:%nexus_pwd% --upload-file %LABEL%.zip  %NEXUS_URL_1%/FM/
 goto end
 :pseutils
 7z a %LABEL%.zip PSEutils.dll -x!fmUpload.bat
+curl  -u jenkucs_sa:%nexus_pwd% --upload-file %LABEL%.zip  %NEXUS_URL_1%/FM/
+goto end
+:fmman
+7z a %LABEL%.zip fm_manager.dll -x!fmUpload.bat
 curl  -u jenkucs_sa:%nexus_pwd% --upload-file %LABEL%.zip  %NEXUS_URL_1%/FM/
 :end
 
