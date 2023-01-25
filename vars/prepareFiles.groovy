28ae8b9a2d09901595c01e71943126335122951a
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-01-26 02:16:54 +0300

    Add files via upload

--- vars/prepareFiles.groovy
+++ vars/prepareFiles.groovy
@@ -1,15 +1,20 @@
 def call(String name) {
     def workspace = WORKSPACE.replaceAll('/','\\\\')
     switch (name) {//local preliminary file operations
-        case ["fis", "mm_nix"] :
+        case ["fis", "mm_nix"] : //fis, mm_nix operations
             loadScript(place:'linux', name:'prepareFiles.sh')
             sh "./prepareFiles.sh"
-            break
-        case ["borland"] :
+        break
+        case ["ppcfm"] : //fm_host operations
+            sh """
+            cp ./git/config/fmux/cfgbuild.mak ./FmUX
+            cp ./git/config/fmux/Makefile ./FmUX/fm """
+        break
+        case ["borland"] : //borland operations
         if (SVN=='trunk' || SVN=='src') 
-        { bat "xcopy ${workspace}\\${SVN} ${workspace} /i /q /d /e & mkdir ${TARGET}" } 
+             { bat "xcopy ${workspace}\\${SVN} ${workspace} /i /q /d /e & mkdir ${TARGET}" } 
         else { bat "xcopy ${workspace}\\${VERSION} ${workspace} /i /q /d /e & mkdir ${TARGET}" }
         break
-        default: println ("Default: fm, chk_sql, scripts2nexus, mmcore/mmlibrary")
+        default: println ("Default: armfm, pseutils, fmman, mm_win")
     }
 }
