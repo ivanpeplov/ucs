ca204fb18691c118c4fba14d19d22842e31d9404
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-03 17:59:14 +0300

    Delete listFilesWin.groovy

--- vars/listFilesWin.groovy
+++
@@ -1,5 +0,0 @@
-def call() { //get filename.ext list from current dir
-    proc = bat (returnStdout: true, script: "dir  /b /o:gn /A-D")
-    fileList = output.split().toList()
-    return fileList  
-}
