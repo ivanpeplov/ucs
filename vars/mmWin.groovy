8cb056a71afd4b436bc4fa8cf3069cc06dbcc6ce
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-03 18:16:12 +0300

    Delete mmWin.groovy

--- vars/mmWin.groovy
+++
@@ -1,9 +0,0 @@
-def call (String yml, String arch) {
-    smpl=yml.split(',')
-    id=arch.split(',')
-    id.each { x-> smpl.each { y-> 
-        bat (script: "cd ${y} & msbuild ${y}.sln /t:build /p:configuration=Release /p:Platform=${x}") 
-        if (y=='ucs_ms') {
-            loadScript(place:'win', name:'mmLib.bat') 
-            bat(script:"mmLib.bat ${y} ${x}" ) } } }
-}
