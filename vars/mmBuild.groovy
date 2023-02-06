3c9d9a85ce3e530232119273faf918e960a4aad6
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-06 17:03:38 +0300

    Add files via upload

--- vars/mmBuild.groovy
+++ vars/mmBuild.groovy
@@ -1,16 +1,16 @@
-def Nix (String yml, String arch) {
+def Nix (String units, String arch) {
     loadScript(place:'linux', name:'mmBuild.sh')
-    smpl=yml.split(',')
-    smpl.each { x-> sh(script:"./mmBuild.sh ${x} ${arch}") }
+    smpl=units.split(',')
+    smpl.each { x-> sh "./mmBuild.sh ${x} ${arch}" }
 }
-def Win (String yml, String arch) {
-    smpl=yml.split(',')
+def Win (String units, String arch) {
+    smpl=units.split(',')
     id=arch.split(',')
     id.each { x-> smpl.each { y-> 
-        bat (script: "cd ${y} & msbuild ${y}.sln /t:build /p:configuration=Release /p:Platform=${x}") 
+        bat "cd ${y} & msbuild ${y}.sln /t:build /p:configuration=Release /p:Platform=${x}"
         if (y=='ucs_ms') {
             loadScript(place:'win', name:'mmLib.bat') 
-            bat(script:"mmLib.bat ${y} ${x}" ) } } }
+            bat "mmLib.bat ${y} ${x}" } } }
 }
 def Fis (String modules, String release) {
     units = modules.split(',').toList()
