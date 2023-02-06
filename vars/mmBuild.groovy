aded666c90cf49d7a3239917ae593353c628966e
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-06 17:11:42 +0300

    Add files via upload

--- vars/mmBuild.groovy
+++ vars/mmBuild.groovy
@@ -1,18 +1,18 @@
 def Nix (String units, String arch) {
     loadScript(place:'linux', name:'mmBuild.sh')
-    smpl=units.split(',')
-    smpl.each { x-> sh "./mmBuild.sh ${x} ${arch}" }
+    modules=units.split(',')
+    modules.each { f-> sh "./mmBuild.sh ${f} ${arch}" }
 }
 def Win (String units, String arch) {
-    smpl=units.split(',')
+    modules=units.split(',')
     id=arch.split(',')
-    id.each { x-> smpl.each { y-> 
-        bat "cd ${y} & msbuild ${y}.sln /t:build /p:configuration=Release /p:Platform=${x}"
+    id.each { x-> modules.each { f-> 
+        bat "cd ${f} & msbuild ${f}.sln /t:build /p:configuration=Release /p:Platform=${x}"
         if (y=='ucs_ms') {
             loadScript(place:'win', name:'mmLib.bat') 
             bat "mmLib.bat ${y} ${x}" } } }
 }
-def Fis (String modules, String release) {
-    units = modules.split(',').toList()
-    units.each {f -> sh "cd ${f} ; Make ${release}"}
+def Fis (String units, String release) {
+    modules = units.split(',')
+    modules.each {f -> sh "cd ${f} ; Make ${release}"}
 }
