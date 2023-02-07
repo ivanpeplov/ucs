ce8aba031c254c68a9496dd415a531c4033ee734
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-07 23:44:03 +0300

    Add files via upload

--- vars/prepareFiles.groovy
+++ vars/prepareFiles.groovy
@@ -1,11 +1,11 @@
 def call(String name) {
     def workspace = WORKSPACE.replaceAll('/','\\\\')
     switch (name) {//local preliminary file operations
-        case ["fis", "mm_nix"] :
-            loadScript(place:'linux', name:'prepareFiles.sh')
-            sh "./prepareFiles.sh"
+        case ["fis", "mm_nix"] : //fis; mm_nix operations
+            loadScript(place:'linux', name:'mmPrepareFiles.sh')
+            sh "./mmPrepareFiles.sh"
         break
-        case ["mm_win"] :
+        case ["mm_win"] : //mm_win operations
             bat "xcopy ${workspace}\\MicroModule ${workspace}\\units /i /q /d /e"
         break
         case ["ppcfm"] : //fm_host operations
@@ -14,9 +14,10 @@ def call(String name) {
             cp ./git/config/fmux/Makefile ./FmUX/fm """
         break
         case ["borland"] : //borland operations
-        if (SVN=='trunk' || SVN=='src') 
-             { bat "xcopy ${workspace}\\${SVN} ${workspace} /i /q /d /e & mkdir ${TARGET}" } 
-        else { bat "xcopy ${workspace}\\${VERSION} ${workspace} /i /q /d /e & mkdir ${TARGET}" }
+        if (SVN=='trunk' || SVN=='src')
+        { bat "xcopy ${workspace}\\${SVN} ${workspace} /i /q /d /e & mkdir ${TARGET}" } 
+        else
+        { bat "xcopy ${workspace}\\${VERSION} ${workspace} /i /q /d /e & mkdir ${TARGET}" }
         break
         default: println ("Default: fm_host (armfm, pseutils, fmman); mm_win")
     }
