2025fa53fcdd56d8557d8982a7c4beb4af9b644f
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-08 00:13:53 +0300

    Add files via upload

--- vars/mmBuild.groovy
+++ vars/mmBuild.groovy
@@ -1,15 +1,14 @@
-// for mm_nix, mm_win, fis
-def Nix (String units) {
+def Nix (String units) { // for mm_nix, fis
     loadScript(place:'linux', name:'mmBuild.sh')
     modules=units.split(',')
     modules.each { m-> sh "./mmBuild.sh ${m} ${OS_ARCH}" }
 }
-def Win (String units) {
+def Win (String units) { // for mm_win
     modules=units.split(',')
     id=arch.split(',')
-    id.each { x-> modules.each { m-> 
-        bat "cd ${m} & msbuild ${m}.sln /t:build /p:configuration=Release /p:Platform=${x}"
+    id.each { a -> modules.each { m-> 
+        bat "cd ${m} & msbuild ${m}.sln /t:build /p:configuration=Release /p:Platform=${a}"
         if (m=='ucs_ms') {
             loadScript(place:'win', name:'mmLib.bat') 
-            bat "mmLib.bat ${m} ${x}" } } }
+            bat "mmLib.bat ${m} ${a}" } } }
 }
