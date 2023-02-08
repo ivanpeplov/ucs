93474fb24a028f39e47042e89cbee0b84c8d49e6
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-08 12:23:10 +0300

    Add files via upload

--- vars/prepareFiles.groovy
+++ vars/prepareFiles.groovy
@@ -6,7 +6,7 @@ def call(String name) {
             sh "./mmPrepareFiles.sh"
         break
         case ["mm_win"] : //mm_win operations
-            bat "xcopy ${workspace}\\MicroModule ${workspace}\\units /i /q /d /e"
+            bat "xcopy .\\MicroModule .\\units /i /q /d /e"
         break
         case ["ppcfm"] : //fm_host operations
             sh """ 
@@ -15,9 +15,9 @@ def call(String name) {
         break
         case ["borland"] : //borland operations
         if (SVN=='trunk' || SVN=='src')
-        { bat "xcopy ${workspace}\\${SVN} ${workspace} /i /q /d /e & mkdir ${TARGET}" } 
+        { bat "xcopy .\\${SVN} . /i /q /d /e & mkdir ${TARGET}" } 
         else
-        { bat "xcopy ${workspace}\\${VERSION} ${workspace} /i /q /d /e & mkdir ${TARGET}" }
+        { bat "xcopy .\\${VERSION} . /i /q /d /e & mkdir ${TARGET}" }
         break
         default: println ("Default: fm_host (armfm, pseutils, fmman); mm_win")
     }
