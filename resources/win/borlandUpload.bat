0f348e1b34584e0db46b8b87ab67f372aa97d932
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-08 15:33:59 +0300

    Add files via upload

--- resources/win/borlandUpload.bat
+++ resources/win/borlandUpload.bat
@@ -2,10 +2,10 @@
 rem borland.groovy
 if not "%~3"=="" goto svn
 :svn
-7z a %1_%2.zip * -x!borlandUpload.bat -x!getWinSVN.bat
+7z a %1_%2.zip * -x!borlandUpload.bat -x!getSVN.bat
 curl -u jenkucs_sa:%nexus_pwd% --upload-file %1_%2.zip  %NEXUS_URL%/Borland/
 goto end
-7z a %1_%2_%3.zip * -x!borlandUpload.bat -x!getWinSVN.bat
+7z a %1_%2_%3.zip * -x!borlandUpload.bat -x!getSVN.bat
 curl -u jenkucs_sa:%nexus_pwd% --upload-file %1_%2_%3.zip  %NEXUS_URL%/Borland/
 :end
                 
