597b912f4a9a2254ad09809bc7cff764362e2093
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-08 14:21:14 +0300

    Add files via upload

--- resources/linux/mmArt.sh
+++ resources/linux/mmArt.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# mm_nix
+# mm_nix.groovy
 cp ${PROJECTS}/lib/*.so ./Linux_Install/UCS_MM/BIN
-cp ${PROJECTS}/bin/ucs_dt ./Linux_Install/UCS_MM/BIN
-cp ${PROJECTS}/bin/ucs_mm ./Linux_Install/UCS_MM/BIN
+cp ${PROJECTS}/bin/{ucs_dt,ucs_mm} ./Linux_Install/UCS_MM/BIN
