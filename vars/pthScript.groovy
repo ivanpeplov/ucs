b8caa6c3489430d130fa23ea217011d1b9592f6a
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-09 09:56:12 +0300

    Update pthScript.groovy

--- vars/pthScript.groovy
+++ vars/pthScript.groovy
@@ -7,7 +7,7 @@ def call(String path) { //chk_sql.groovy
         sh "./spaceToUnderscore.sh; find . -type d -name .svn -exec rm -rf {} + "
         loadScript(place:'linux', name:'pthUpload.sh') // bash
         loadScript(place:'linux', name:'pthChecker.sh') // ktr2xml + checkerSQL
-        loadScript(place:'linux', name:'xdbChecker.sh') // sql checker
+        loadScript(place:'linux', name:'xdbChecker.sh') // only checkerSQL
         lvl2 = listDir.Nix("${path}") - 'BIN' //[AMSBatch.PTH, BonusETL.PTH, ..., NTPREFS.XDB] - 'BIN'
         for (jo in lvl2) {
             ext = jo.substring(jo.indexOf(".")+1); // [PTH, PTH, ..., XDB]
