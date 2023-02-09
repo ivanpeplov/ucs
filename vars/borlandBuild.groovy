031593ddba6cc2d8f2e05e9a8d8db92535753aa8
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-10 01:58:32 +0300

    Add files via upload

--- vars/borlandBuild.groovy
+++ vars/borlandBuild.groovy
@@ -1,22 +1,22 @@
-def call(String rt, String label) { //borland groovy
-    dir (rt) {
-        switch (label) {  //module selection
-            case ["cardpro", "mmseod", "palmerauloade", "passkey"] :
-                bat "make -f ${label}.mak & xcopy ${label}.exe ${TARGET}"
+def call(String workdir, String module) { //borland groovy
+    dir (workdir) {
+        switch (module) {  //module selection
+            case ["cardpro", "mmseod", "palmerauloade", "passkey"] : //all builds
+                bat "make -f ${module}.mak & xcopy ${module}.exe ${TARGET}"
                 bmp.split(',').each { f -> bat "xcopy ${f} ${TARGET}"}
             break
-            case ["bin", "32"] : 
+            case ["bin", "32"] : //mmseod and cardpro
                 bpl.split(',').each { f -> bat "xcopy ${f} ${TARGET}" }
             break
-            case ("cardlib") : 
-                bat "make -f ${label}.mak & xcopy C:\\bpl\\*.bpl ${TARGET}"
+            case ("cardlib") : //cardpro
+                bat "make -f ${module}.mak & xcopy C:\\bpl\\*.bpl ${TARGET}"
             break 
-            case ("form") :
+            case ("form") : //cardpro
                 dir = listDir.Win("form") - 'TEMPLATE'
                 for (io in dir) {
                     if (io=='PRINT.CFG')
-                    { frm.split(',').each {f->bat "xcopy .\\${io}\\${f} ${TARGET}\\${rt}\\${io}\\"} }
-                    else { bat "cd ${io} & make -f ${io}.mak & xcopy ${io}.dll ${TARGET}\\${rt}\\${io}\\" } 
+                    { frm.split(',').each {f->bat "xcopy .\\${io}\\${f} ${TARGET}\\${workdir}\\${io}\\"} }
+                    else { bat "cd ${io} & make -f ${io}.mak & xcopy ${io}.dll ${TARGET}\\${workdir}\\${io}\\" } 
                 }
             break
         }
