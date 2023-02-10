c6be735ab6ef2c5bdaffdc8e48be0bec8da70253
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-10 14:41:54 +0300

    Add files via upload

--- vars/pthConversion.groovy
+++ vars/pthConversion.groovy
@@ -1,10 +1,10 @@
-//for pthscript.groovy
+//called from pthscript.groovy
 import org.apache.commons.io.FilenameUtils
 def call (Map pth=[:]) { //PTH, XDB conversion
     pth.get('l3', '') // default value for map. change 'null' to ''
     if (pth.todo=='PTH') //filter .ktr/.kjb or .xml files
-    {list = listFiles.Nix ("${pth.l1}/${pth.l2}/${pth.l3}").findAll { it=~ /(?i)\.(?:ktr|kjb)$/ } } //get all .ktr/.kjb
-    else {list = listFiles.Nix ("${pth.l1}/${pth.l2}/${pth.l3}").findAll { it=~ /(?i)\.(?:xml)$/ } } //get all .xml
+    { list = listFiles.Nix ("${pth.l1}/${pth.l2}/${pth.l3}").findAll { it=~ /(?i)\.(?:ktr|kjb)$/ } } //get all .ktr/.kjb
+    else { list = listFiles.Nix ("${pth.l1}/${pth.l2}/${pth.l3}").findAll { it=~ /(?i)\.(?:xml)$/ } } //get all .xml
     for (i in list) {
         extension  = FilenameUtils.getExtension(i) //each filename
         name = FilenameUtils.removeExtension(i) //each extension
