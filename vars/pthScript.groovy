b0acfb4ed3657892985074be3b4098221a35d298
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-11 01:47:00 +0300

    Add files via upload

--- vars/pthScript.groovy
+++ vars/pthScript.groovy
@@ -3,19 +3,22 @@ def call(String path) { //chk_sql.groovy
         def nexus_creds = [ //masking nexus credentials
             [ path: 'secrets/creds/nexus', secretValues: [
             [ envVar: 'nexus_pwd', vaultKey: 'password']]]]
+
         loadScript(place:'linux', name:'spaceToUnderscore.sh')
         sh "./spaceToUnderscore.sh; find . -type d -name .svn -exec rm -rf {} + "
         loadScript(place:'linux', name:'pthUpload.sh') // bash
         loadScript(place:'linux', name:'pthChecker.sh') // ktr2xml + checkerSQL
         loadScript(place:'linux', name:'xdbChecker.sh') // sql checker
+        
         lvl2 = listDir.Nix("${path}") - 'BIN' //[AMSBatch.PTH, BonusETL.PTH, ..., NTPREFS.XDB] - 'BIN'
         for (itLvl2 in lvl2) {
-            exe = itLvl2.substring(itLvl2.indexOf(".")+1); // [PTH, PTH, ..., XDB]
-            lvl3 = listDir.Nix("${path}/${itLvl2}") 
-            if (lvl3 != '') { for (itLvl3 in lvl3) // if has folders - recursively process them
-            { pthConversion (todo:"${exe}", l1:"${path}", l2:"${itLvl2}", l3:"${itLvl3}") } /*substage recursion*/
-            } //stage conversion
-            pthConversion (todo:"${exe}", l1:"${path}", l2:"${itLvl2}") /*stage no recursion*/ 
+            exe = itLvl2.substring (itLvl2.indexOf(".") +1) // [PTH, PTH, ..., XDB]
+            lvl3 = listDir.Nix ("${path}/${itLvl2}") 
+            if (lvl3 != '') {
+                for (itLvl3 in lvl3) // if has folders - recursively process them
+                { pthConversion (todo:"${exe}", l1:"${path}", l2:"${itLvl2}", l3:"${itLvl3}") } // lvl3 recursion
+            } //lvl3 conversion
+            pthConversion (todo:"${exe}", l1:"${path}", l2:"${itLvl2}") // no lvl3 recursion
             wrap([$class: 'VaultBuildWrapper', vaultSecrets: nexus_creds]) 
             { sh "./pthUpload.sh ${itLvl2}" } // upload to nexus
         }
