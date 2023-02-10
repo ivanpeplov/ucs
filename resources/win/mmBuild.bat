ef3ef13175ece409ba5fda0c591126cb3b54f118
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-10 12:17:25 +0300

    Add files via upload

--- resources/win/mmBuild.bat
+++ resources/win/mmBuild.bat
@@ -4,8 +4,6 @@ rem micros.lib to ucs.ms_lib - prelim step for ucs_dt.exe building && fix at ucs
 cd %1
 msbuild %1.sln /t:build /p:configuration=Release /p:Platform=%2
 if not %1==ucs_ms goto EOF
-rem if %1==ucs_ms goto lib
-rem :lib
 if %2==x64 ( copy .\Release\micros.lib ..\..\samples\test\bin64\ucs_ms.lib & sed -i s/bin\\ucs_ms.lib/bin64\\ucs_ms.lib/ ..\ucs_dt\ucs_dt.vcxproj)
 if %2==Win32 ( copy .\Release\micros.lib ..\..\samples\test\bin\ucs_ms.lib )
 :EOF
