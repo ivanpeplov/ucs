066e5d8572a8baf79ccd61d60f58d889b8a89d97
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-12 16:29:02 +0300

    Add files via upload

--- vars/pthScript.groovy
+++ vars/pthScript.groovy
@@ -6,12 +6,12 @@ def call(String path) { //chk_sql.groovy
         listSQL.split(',').each {s -> loadScript(place:'linux', name: "${s}")}
         sh "./spaceToUnderscore.sh; find . -type d -name .svn -exec rm -rf {} + "
         lvl2 = listDir.Nix("${path}") - 'BIN' //[AMSBatch.PTH, BonusETL.PTH, ..., NTPREFS.XDB] - 'BIN'
-        for (itLvl2 in lvl2) {
+        lvl2.each { itLvl2 ->
             exe = itLvl2.substring (itLvl2.indexOf(".") +1) // [PTH, PTH, ..., XDB]
             lvl3 = listDir.Nix ("${path}/${itLvl2}") 
             if (lvl3 != '') {
-                for (itLvl3 in lvl3) // if has folders - recursively process them
-                { pthConversion (todo:"${exe}", l1:"${path}", l2:"${itLvl2}", l3:"${itLvl3}") } // lvl3 recursion
+                lvl3.each { itLvl3 -> // if has folders - recursively process them
+                pthConversion (todo:"${exe}", l1:"${path}", l2:"${itLvl2}", l3:"${itLvl3}") } // lvl3 recursion
             } //lvl3 conversion
             pthConversion (todo:"${exe}", l1:"${path}", l2:"${itLvl2}") // no lvl3 recursion
             wrap([$class: 'VaultBuildWrapper', vaultSecrets: nexus_creds]) 
