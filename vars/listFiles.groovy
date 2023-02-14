44f7e9ee3cfd00513a611c00706f4ad49fce5d8c
IvanPeplov <114183110+ivanpeplov@users.noreply.github.com>
2023-02-14 17:30:09 +0300

    Update listFiles.groovy

--- vars/listFiles.groovy
+++ vars/listFiles.groovy
@@ -1,7 +1,7 @@
 //for all jenkinsfile
 def Nix(String dir) { //get filename.ext list from current dir
-    full_dir="${WORKSPACE}/${dir}"
-    def output = sh returnStdout: true, script: "ls -l ${full_dir} | grep -v '^d' | awk '{a=match(\$0, \$9); print substr(\$0,a)}' | tail -n +2"
+    full_path="${WORKSPACE}/${dir}"
+    def output = sh returnStdout: true, script: "ls -l ${full_path} | grep -v '^d' | awk '{a=match(\$0, \$9); print substr(\$0,a)}' | tail -n +2"
     fileList = output.split().toList() //sed -n '/.k[j-tb-r:]/p' --> select files with .ktr/.kjb extension
     return fileList
 }
